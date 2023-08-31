5a2c75c4bf165d66b57406c966efb71e76cb4a41
Davy Mitchell <daftspaniel@gmail.com>
2023-08-31 20:01:26 +0100

    First version.

---
+++ README.md
@@ -0,0 +1,6 @@
+# SGEdit SepTandy 2023
+
+For SeptTandy I am reworking SGEdit into a React web app.
+
+Follow along on the blog:
+https://davysretrocorner.blogspot.com/
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>SG-Edit SepTandy 2023</h1>
+      <div className="card">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
