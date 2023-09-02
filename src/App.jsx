9a3faf64ff36d32fb7e2cdce847b8a8d1b38d632
Davy Mitchell <daftspaniel@gmail.com>
2023-09-02 08:11:53 +0100

    Create Palettes component. Make Prettier config JSON.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css'
-import Palette from './Components/Palette'
+import Palettes from './Components/Palettes'
 
 const App = () => {
   return (
     <div>
       <h1>SG-Edit SepTandy 2023</h1>
-      <div className="Palettes">
-      <Palette range={{ start: 128, end: 256 }} />
-      <Palette range={{ start: 0, end: 128 }} />
-      </div>
+      <Palettes />
     </div>
   )
 }
---
+++ src/Components/Palettes.jsx
@@ -0,0 +1,12 @@
+import Palette from './Palette'
+
+const Palettes = () => {
+  return (
+    <div className="Palettes">
+      <Palette range={{ start: 128, end: 256 }} />
+      <Palette range={{ start: 0, end: 128 }} />
+    </div>
+  )
+}
+
+export default Palettes
